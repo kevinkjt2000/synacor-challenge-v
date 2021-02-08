5484f52c16a8ab33cbb2b1507e375f6ffd85302c
Kevin Tindall <kevinkjt2000@gmail.com>
2021-02-07 21:14:02 -0600

    Extract machine code into struct to simplify main

--- main.v
+++ main.v
@@ -8,35 +8,48 @@ enum Operation {
 	nop = 21
 }
 
-fn main() {
-	mut memory := [32768]MemoryWord{}
-	mut registers := [8]MemoryWord{}
-	mut stack := []MemoryWord{cap: 1000}
-	byte_code := os.read_file("challenge.bin")?
+struct Machine {
+mut:
+	memory [32768]MemoryWord
+	registers [8]MemoryWord
+	stack []MemoryWord = []MemoryWord{cap:1000}
+	pc int
+}
+
+fn (mut m Machine) load(path string) ? {
+	byte_code := os.read_file(path) ?
 	for i in 0..(byte_code.len / 2) {
 		low_byte := MemoryWord(byte_code[2*i])
 		high_byte := MemoryWord(byte_code[2*i + 1])
-		memory[i] = MemoryWord((high_byte << 8) + low_byte)
+		m.memory[i] = MemoryWord((high_byte << 8) + low_byte)
 	}
-	mut pc := 0
+}
+
+fn (mut m Machine) run() {
 	for {
-		op := memory[pc]
+		op := m.memory[m.pc]
 		match Operation(op) {
 			.out {
-				arg := memory[pc+1]
+				arg := m.memory[m.pc+1]
 				print(rune(arg))
-				pc += 2
+				m.pc += 2
 			}
 			.halt {
 				break
 			}
 			.nop {
-				pc++
+				m.pc++
 			}
 			else {
 				println("op($op) has not been coded yet")
-				pc++
+				m.pc++
 			}
 		}
 	}
+}
+
+fn main() {
+	mut m := Machine{}
+	m.load("challenge.bin") ?
+	m.run()
 }
