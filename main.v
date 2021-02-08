cf63cdcba20664e970ef310afd659f506fc2c02a
Kevin Tindall <kevinkjt2000@gmail.com>
2021-02-07 21:47:15 -0600

    Implement jmp

--- main.v
+++ main.v
@@ -1,6 +1,7 @@
 import os
 
 type MemoryWord = u16
+
 enum Operation {
 	halt = 0
 	set = 1
@@ -28,17 +29,17 @@ enum Operation {
 
 struct Machine {
 mut:
-	memory [32768]MemoryWord
+	memory    [32768]MemoryWord
 	registers [8]MemoryWord
-	stack []MemoryWord = []MemoryWord{cap:1000}
-	pc int
+	stack     []MemoryWord = []MemoryWord{cap: 1000}
+	pc        int
 }
 
 fn (mut m Machine) load(path string) ? {
 	byte_code := os.read_file(path) ?
-	for i in 0..(byte_code.len / 2) {
-		low_byte := MemoryWord(byte_code[2*i])
-		high_byte := MemoryWord(byte_code[2*i + 1])
+	for i in 0 .. (byte_code.len / 2) {
+		low_byte := MemoryWord(byte_code[2 * i])
+		high_byte := MemoryWord(byte_code[2 * i + 1])
 		m.memory[i] = MemoryWord((high_byte << 8) + low_byte)
 	}
 }
@@ -47,9 +48,13 @@ fn (mut m Machine) run() {
 	for {
 		op := m.memory[m.pc]
 		match Operation(op) {
+			.jmp {
+				a := m.memory[m.pc + 1]
+				m.pc = a
+			}
 			.out {
-				arg := m.memory[m.pc+1]
-				print(rune(arg))
+				a := m.memory[m.pc + 1]
+				print(rune(a))
 				m.pc += 2
 			}
 			.halt {
@@ -59,7 +64,7 @@ fn (mut m Machine) run() {
 				m.pc++
 			}
 			else {
-				println("op($op) has not been coded yet")
+				println('op($op) has not been coded yet')
 				m.pc++
 			}
 		}
@@ -68,6 +73,6 @@ fn (mut m Machine) run() {
 
 fn main() {
 	mut m := Machine{}
-	m.load("challenge.bin") ?
+	m.load('challenge.bin') ?
 	m.run()
 }
